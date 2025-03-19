80eddd173205df2d72af71d29dbb73ea81eab1af
Casblasvic <casblasvic@gmail.com>
2025-03-19 00:18:28 +0000

    Creacion de Dashboard de ejemplo

---
+++ components/ui/date-range-picker.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { CalendarIcon } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { cn } from "@/lib/utils"
+import { format } from "date-fns"
+import { es } from "date-fns/locale"
+import { DateRange } from "react-day-picker"
+import { useState } from "react"
+
+export function CalendarDateRangePicker() {
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000),
+    to: new Date(),
+  })
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          id="date"
+          variant={"outline"}
+          className="w-[300px] justify-start text-left font-normal"
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {dateRange?.from ? (
+            dateRange.to ? (
+              <>
+                {format(dateRange.from, "d MMM, yyyy", { locale: es })} -{" "}
+                {format(dateRange.to, "d MMM, yyyy", { locale: es })}
+              </>
+            ) : (
+              format(dateRange.from, "d MMM, yyyy", { locale: es })
+            )
+          ) : (
+            <span>Selecciona un rango</span>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0" align="start">
+        <Calendar
+          initialFocus
+          mode="range"
+          defaultMonth={dateRange?.from}
+          selected={dateRange}
+          onSelect={setDateRange}
+          locale={es}
+          numberOfMonths={2}
+        />
+      </PopoverContent>
+    </Popover>
+  )
+} 
